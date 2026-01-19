31f142d80b1d741c2e9e6eab3c41a183005b799f
juanpablozorrilla06 <juanpablozorrilla06@gmail.com>
2026-01-19 15:22:12 +0100

    Delete create_shared_state function from shared.py
    Remove the create_shared_state function and its imports.

--- shared.py
+++ shared.py
@@ -1,12 +1,1 @@
-from multiprocessing import Manager, Lock
 
-def create_shared_state():
-    manager = Manager()
-    state = manager.dict({
-        "grass": 100,
-        "num_predators": 0,
-        "num_preys": 0,
-        "drought": False
-    })
-    lock = manager.Lock()
-    return state, lock
